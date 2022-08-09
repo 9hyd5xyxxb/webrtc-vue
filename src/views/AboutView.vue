7449b22c5c5febb746173919c1e785df9a56347f
9hyd5xyxxb <9hyd5xyxxb@mail.ru>
2022-08-09 09:26:31 +0500

    call the kraken!

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+    <vue-webrtc ref="webrtc" width="100%" roomId="sample-room"></vue-webrtc>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
